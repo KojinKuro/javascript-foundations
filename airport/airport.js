52e875eaa63aa5b75e8b8f4dde93d754cb7ca709
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-02-05 14:15:44 -0800

    Complete airport.js with tests

--- airport/airport.js
+++ airport/airport.js
@@ -1,9 +1,41 @@
+createAirport('Denver International Airport', ['United', 'Southwest', 'Delta'], 144)
 
+function createAirport(name, airlines, availableGates) {
+  return { name, airlines, availableGates };
+}
 
+function welcomeGuests(airport) {
+  return `Welcome to ${airport.name}!`;
+}
+
+function landPlanes(airport, gateCount) {
+  airport.availableGates -= gateCount;
+
+  if(airport.availableGates > 0) {
+    airport.message = `Success! Current availability is ${airport.availableGates}.`
+  } else {
+    let overflow = Math.abs(airport.availableGates);
+    airport.availableGates = 0;
+    airport.message = `Oh no! Not enough gates available. Current overflow is ${overflow}.`;
+  }
+
+  return airport;
+}
+
+function checkAirlineLocations(airports, airline) {
+  let arrayOfAirlines = [];
+  for(let i=0; i < airports.length; ++i) {
+    if (airports[i].airlines.find((ar) => ar === airline)) {
+      arrayOfAirlines.push(airports[i].name);
+    }
+  }
+
+  return arrayOfAirlines
+}
 
 module.exports = { 
-  // createAirport, 
-  // welcomeGuests, 
-  // landPlanes, 
-  // checkAirlineLocations
+  createAirport, 
+  welcomeGuests, 
+  landPlanes, 
+  checkAirlineLocations
 };
