7db4e180a6d9dc2be78d070bd92ccb700c23681f
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-02-06 11:32:56 -0800

    wip: Finish some of crafts.js

--- crafting/crafts.js
+++ crafting/crafts.js
@@ -1,10 +1,32 @@
+function createMaterial(name,unit,costPerUnit) {
+  return {name,unit,costPerUnit}
+}
 
+function calculateMaterialCost(material,amount) {
+  return `${amount} ${material.unit}s of ${material.name} costs $${amount*material.costPerUnit}.`;
+}
+
+// var myCloset = createSupplyCloset([fabric, paint, yarn]);
+
+// assert.deepEqual(myCloset, { supplies: ['fabric', 'paint', 'yarn'] });
+
+function createSupplyCloset(items) {
+  var closet = { supplies: [] };
+  if (arguments.length) items.map((item) => closet.supplies.push(item.name));
+
+  return closet;
+}
+
+function addSupply(closet,item) {
+  closet.supplies.push(item.name);
+  return closet.supplies;
+}
 
 module.exports = {
-  // createMaterial,
-  // calculateMaterialCost,
-  // createSupplyCloset,
-  // addSupply,
+  createMaterial,
+  calculateMaterialCost,
+  createSupplyCloset,
+  addSupply,
   // createNewProject,
   // compareMaterials
 }
