cd3414c379adadfc68fc62ee6057b7efba09ed84
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-02-16 23:06:24 -0800

    Complete spotify with tests

--- spotify/spotify.js
+++ spotify/spotify.js
@@ -1,4 +1,46 @@
+function createCollection(name, description) {
+  return { name, description, tracks: [] };
+}
 
+function createTrack(title = "unknown", artist = "unknown", duration = 0) {
+  return { title, artist, duration };
+}
 
+function reviewTrack(track) {
+  var awesomeBand = "Red Hot Chili Peppers";
 
-  module.exports = {  }
+  if (track.artist === awesomeBand) return `The song ${track.title} rules!`;
+  return `I wish this was a ${awesomeBand} song.`;
+}
+
+function addTrack(collection, track) {
+  var newCollection = JSON.parse(JSON.stringify(collection));
+  newCollection.tracks.push(track);
+  return newCollection;
+}
+
+function getTotalDuration(collection) {
+  var totalDuration = 0;
+  collection.tracks.forEach((track) => {
+    totalDuration += track.duration;
+  });
+  return totalDuration;
+}
+
+function findTracksByArtist(collection, artist) {
+  var matchTracks = [];
+  collection.tracks.forEach((track) => {
+    if (track.artist === artist) matchTracks.push(track);
+  });
+
+  return matchTracks;
+}
+
+module.exports = {
+  createCollection,
+  createTrack,
+  reviewTrack,
+  addTrack,
+  getTotalDuration,
+  findTracksByArtist,
+};
